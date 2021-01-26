8c190801d0fe927b0c039d3daa684575df9049d2
AhmedSamyMerah <meraha@sheridancollege.ca>
2021-01-25 21:33:40 -0500

    Added url and normalization of DataFrame

--- mlbSingleDayToCsv.py
+++ mlbSingleDayToCsv.py
@@ -8,6 +8,12 @@ import click
 @click.argument('date')
 def main(sport, date):
     print(sport, date)
+    try:
+        url = 'https://statsapi.mlb.com/api/v1/schedule?sportId=%s&date=%s&hydrate=linescore' %(sport, date)
+        df = pd.read_json(url)
+        print(df.head())
+    except:
+        print('Did not work bihhh')
 
 if __name__ == '__main__':
     main()
