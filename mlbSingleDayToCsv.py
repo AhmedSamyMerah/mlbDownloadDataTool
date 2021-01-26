30e93e2bf65179beb8ac9b72aefb53accfc19fdb
AhmedSamyMerah <meraha@sheridancollege.ca>
2021-01-25 22:15:58 -0500

    Downloads CSV in current directory adn has success and error messages

--- mlbSingleDayToCsv.py
+++ mlbSingleDayToCsv.py
@@ -7,13 +7,18 @@ import click
 @click.argument('sport')
 @click.argument('date')
 def main(sport, date):
-    print(sport, date)
     try:
         url = 'https://statsapi.mlb.com/api/v1/schedule?sportId=%s&date=%s&hydrate=linescore' %(sport, date)
         df = pd.read_json(url)
-        print(df.head())
+        df1 = pd.json_normalize(df['dates'])
+        df2 = pd.json_normalize(np.hstack(df1['games']))
+        df2.to_csv('gameDay_%s.csv' %(date))
+        print('File successfully saved in the current directory!')
     except:
-        print('Did not work bihhh')
-
+        print('-------------')
+        print('Please make sure your arguments follow the following convention: mlbSingleDayToCsv.py 1 2020-07-27')
+        print('First argument is sportId and the second argument is the date that follows YYYY-MM-DD including the dashes')
+        print('-------------')
+        
 if __name__ == '__main__':
     main()
